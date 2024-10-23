ec7472a1ee507e3ad1bb74a9880f0d63d4803bd9
Niklas Pelli <niklaspelli22@gmail.com>
2024-10-23 09:29:51 +0200

    added taskController

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -14,3 +14,58 @@ const [result] = await pool.query(
 res.status(500).json({ error: "Failed to create new task"});
 	}
 };
+
+const getTaskForBoard = async (req, res) => {
+	const boardId = req.params.boardId;
+	try{
+            const [tasks] = await pool.query(`SELECT * FROM tasks WHERE board_id`);
+		res.status(200).json(tasks);
+         	} catch (error) {
+ 		res.status(500).json({ error: "Failed to get tasks"})
+		}
+};
+
+const openTask = async (req, res) => {
+	const boardId = req.params.boardId;
+	const taskId = req.params.taskId;
+	try {
+	     const [taskRows] = await pool.query(
+	     `SELECT * FROM tasks WHERE id = ? AND board_id = ?`, [taskId, boardId],
+	     );
+		if (taskRows.length === 0) {
+		return res.status(404).json({ message: "Task not found"});
+		}
+		const [comment] = await pool.query(
+		`SELECT * from task_comments WHERE task_id = ?`, [taskId],
+		);
+		res.status(200).json({
+			task: taskRows[0],
+			comments: comments,
+		});
+	} catch (error) {
+		res.status(500).json({ res.status({ error: "Failed to open selected task"});      
+}
+
+};
+
+const addTaskComment = async (req, res) => {
+	const taskId = req.params.taskId;
+	const { comment } = req.body;
+	const userId = req.user.userId;
+
+	try {
+	  await pool.query(
+	  "INSERT INTO task_comments (task_id, comment, user_id) VALUES (?, ?, ?)",
+		  [taskId, comment, userId]
+	  );
+		res.status(201).json({ message: "Comment added to task" });
+	} catch (error) {
+		res.status(500).json({ error: "Failed to add comment to selected task" });
+	}
+};
+
+
+module.exports = { createTask, getTaskForBoard, openTask, addTaskComment };
+
+
+
